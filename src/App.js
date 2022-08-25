dbb0ce589a1c4332ba7ecab17d704213c29c9394
Vance97 <vansmuchongo@gmail.com>
2022-08-25 23:31:42 +0200

    UploadImage Part

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
+import { storage } from "./firebase.js";
+import {ref, uploadBytes, listAll, getDownloadURL} from "firebase/storage";
+import { v4 } from "uuid";
 
 function App() {
+  const [imageUpload, setImageUpload] = useState(null);
+  const [imageList, setImageList] = useState([]);
+
+  const imageListRef = ref(storage, "Product-Images/")
+  const uploadImage = () => {
+    if (imageUpload == null) return;
+    const imageRef = ref(storage, `Product-Images/${imageUpload.name+v4()}`);
+    uploadBytes(imageRef, imageUpload).then((snapshot) =>{
+      // alert("Image Uploaded");
+      getDownloadURL(snapshot.ref).then((url) => {
+        setImageList((prev) => [...prev,url]);
+      }) 
+    })
+  }; 
+
+  useEffect(()=>{
+    listAll(imageListRef).then((response) => {
+      response.items.forEach((item) =>{
+        getDownloadURL(item).then((url) => {
+          setImageList((prev) => [...prev, url]);
+        })
+      })
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App">
+    <input type="file" onChange={(event) => {setImageUpload(event.target.files[0])}} />
+    <button onClick={uploadImage}>Upload Image</button>
+    {imageList.map((url) => {
+      return <img src={url}/>
+    })}
+  </div>
   );
+         
 }
 
 export default App;
