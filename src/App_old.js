d1dfcd6bcc4dc8a4c5c302a9a1733f6126706d46
Vance97 <vansmuchongo@gmail.com>
2022-09-03 11:56:08 +0200

    LoginSignupSellPage

---
+++ src/App_old.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { Home } from './Components/Home'
+import { Login } from './Components/Login'
+import { Signup } from './Components/Signup'
+import { NotFound } from './Components/NotFound'
+import { AddProducts } from './Components/AddProducts'
+import { Cart } from './Components/Cart'
+
+export const App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component = {Home}/>
+        <Route path="/signup" component={Signup}/>
+        <Route path="/login" component={Login}/>
+        <Route path="/add-products" component={AddProducts}/>
+        <Route path="/cart" component={Cart}/>       
+        <Route component={NotFound}/>        
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default App
