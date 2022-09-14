30613ecdfa7e8b3642ca53a7fccae00903b2e9e3
Vance97 <vansmuchongo@gmail.com>
2022-09-14 20:37:24 +0200

    downgraded @testing-library/react to 12

--- src/Components/AddProducts/__test__/AddProducts.test.js
+++ src/Components/AddProducts/__test__/AddProducts.test.js
@@ -1,4 +1,16 @@
 import { AddProducts } from "../AddProducts";
 import React from "react";
-import { ReactDOM } from "react-dom";
+import TestRenderer from 'react-test-renderer';
+import {render, fireEvent} from '@testing-library/react';
 
+
+test('renders the form properly',()=>{
+  const {getByTestId, getByLabelText} = render(<App/>)
+
+  const titleLabel = getByText(/Product Title:/);
+  const descriptionLabel = getByText(/Product Description:/);
+
+  expect(titleLabel).toBeInDocument();
+  expect(descriptionLabel).toBeInDocument();
+
+})
