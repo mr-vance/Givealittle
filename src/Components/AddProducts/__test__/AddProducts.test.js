b81c28fedf8b12168954109d3d6eebfe9c1ed60f
Vance97 <vansmuchongo@gmail.com>
2022-09-14 23:16:45 +0200

    Test for Form Labels Passing

--- src/Components/AddProducts/__test__/AddProducts.test.js
+++ src/Components/AddProducts/__test__/AddProducts.test.js
@@ -3,10 +3,22 @@ import React from "react";
 import ReactDOM from 'react-dom';
 import tsAnyKeyword from "@babel/types"
 import TestRenderer from 'react-test-renderer';
-import {render, fireEvent, screen} from '@testing-library/react';
+import {render, fireEvent, screen, getByText, configure, getByTextId} from '@testing-library/react';
+import { toHaveAttribute } from "@testing-library/jest-dom/dist/matchers";
 
 
-it("renders without crashing", ()=>{
-  const div = document.createElement("div");
-  ReactDOM.render(<label></label>, div);
+// it("renders without crashing", ()=>{
+//   const div = document.createElement("div");
+//   ReactDOM.render(<label></label>, div);
+// })
+
+test("renders product title and description text", ()=>{
+  const {getByTestId, getByLabelText} = render(<AddProducts/>);
+
+  const titleLabel = screen.getByText('Product Title');
+  const titleDescr = screen.getByText('Product Description');
+  expect(titleLabel).toBeInTheDocument();
+  expect(titleDescr).toBeInTheDocument();
+  // const input = screen.getByLabelText("Product Title");
+  // expect(input).toHaveAttribute('type','text');
 })
