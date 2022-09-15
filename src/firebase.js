d5dd54672a10fb777113dffcd09534cdcbe0f598
Thibello-Khaile <101095268+Thibello-Khaile@users.noreply.github.com>
2022-09-15 15:06:06 +0200

    forgot password working

--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,16 @@
-import firebase from 'firebase'
-import 'firebase/auth'
-import 'firebase/firestore'
-import 'firebase/storage'
+import firebase from 'firebase/compat/app'
+import "firebase/compat/auth"
 
-const firebaseConfig = {
+const app = firebase.initializeApp({
   apiKey: "AIzaSyBs7ggwCZe1kZU-_8tK3hENM8mR4F4nE0o",
   authDomain: "give-a-little-d525a.firebaseapp.com",
+  databaseURL: "https://give-a-little-d525a-default-rtdb.firebaseio.com",
   projectId: "give-a-little-d525a",
   storageBucket: "give-a-little-d525a.appspot.com",
   messagingSenderId: "392348497447",
   appId: "1:392348497447:web:11be1695e3764c3122c002",
   measurementId: "G-VTY0MM9XV4"
-};
+})
 
-firebase.initializeApp(firebaseConfig);
-
-const auth = firebase.auth();
-const fs = firebase.firestore();
-const storage = firebase.storage();
-
-export {auth,fs,storage}
+export const auth = app.auth()
+export default app
