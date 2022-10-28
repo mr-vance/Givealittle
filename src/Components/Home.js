5d8b617b3fc7a6b3d492c27daab863325506c7d1
Vance97 <vansmuchongo@gmail.com>
2022-10-28 05:49:48 +0200

    error fix

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,4 +1,5 @@
-import React,{useState, useEffect, useRef, lazy} from 'react'
+import React,{useState, useEffect, useRef} from 'react'
+//import { Navbar } from './Navbar'
 import { Products } from './Products'
 import {auth,fs} from '../Config/Config'
 import {Link} from 'react-router-dom'
@@ -6,9 +7,7 @@ import logo from '../Images/logo.png'
 import {Icon} from 'react-icons-kit'
 import {shoppingCart} from 'react-icons-kit/feather/shoppingCart'
 import {useHistory} from 'react-router-dom'
-import CategorySelector from './CategorySelector';
-import CategorySearchDropdown from './CategorySearchDropdown';
-import SearchSuggestion from './SearchSuggestion';
+import { AiOutlineHeart } from "react-icons/ai"
 
 
 export const Home = (props) => {
@@ -20,15 +19,6 @@ export const Home = (props) => {
     const searchRef = useRef();
     
     const [searchTerm, setSearchTerm] = useState("");
-
-
-    function StarRAting(){
-      return(
-        <div className='App'>
-              <StarRating />
-          </div>
-      )
-    }
 
     function Navbar(){
         const history = useHistory();
@@ -50,16 +40,12 @@ export const Home = (props) => {
                         </Link>
                     </div>
                 </div>
-
-
                 <div className='rightside'>
     
                     {!user&&<>
-                    
                         <div><Link className='navlink' to="signup">SIGN UP</Link></div>
                         <div><Link className='navlink' to="login">LOGIN</Link></div>
                     </>} 
-                    
                     
                     <input type="text"
                   placeholder="Search..."
@@ -73,14 +59,7 @@ export const Home = (props) => {
                 </button>
     
                     {user&&<>
-
-                      <div className='centerside'>
-                  <div><Link className='navlink' to="/add-products" style={{color: 'white'}} >Are you a seller?</Link></div>
-                </div>
-                    
                         <div><Link className='navlink' to="/">{user}</Link></div>
-                        
-                        
                         <div className='cart-menu-btn'>
                             <Link className='navlink' to="cart">
                                 <Icon icon={shoppingCart} size={20}/>
@@ -230,7 +209,7 @@ export const Home = (props) => {
           <div>
     
             <div className="item-container">
-              <button className='btn btn-danger btn-md' onClick={() => setShow(false)}>Close</button>
+              <button className="cart-btn" onClick={() => setShow(false)}>Close</button>
     
               <div>
                 <img style={{ boxShadow: "0px 0px 10px 0px rgb(200, 200, 200)" }} src={item.url} />
@@ -241,9 +220,8 @@ export const Home = (props) => {
               <h1 className="product-view-price">R{item.price}</h1>
               <div>
                 <input type="number" className="edtnum" placeholder="1" min='0' max={item.Quantity} />
-                <button className='btn btn-danger btn-md' onClick={() => addToCart(item)}>Add to cart</button>
-                <button className='btn btn-danger btn-md' onClick={() => addToWishlist(item)}>Add to wishlist</button>
-                
+                <button className="cart-btn" onClick={() => addToCart(item)}>Add to cart</button>
+                <button className="cart-btn" onClick={() => addToWishlist(item)}>Add to wishlist</button>
               </div>
     
             </div>
@@ -257,15 +235,9 @@ export const Home = (props) => {
 
 
       return (
-        
         <div>
-          
-          
           <Navbar />
-          <BootstrapCarousel />
-          
           {
-            
             show ? <div className="reviewdiv">
               {text}
             </div> :
@@ -279,19 +251,16 @@ export const Home = (props) => {
                 }).map((item) => {
                   return <div className="itemdiv" onClick={() => {
                     ProductView(item)
-                    
                   }}>
                     <img src={item.url} alt="nope" />
                     <div className="textdiv">
                       <h1 className="itemname">{item.title}</h1>
                     </div>
                     <h1 className="itemprice">R{item.price}</h1>
-                    
                     {(() => {
                       if (item.Quantity == 0) {
                         return (
                           <h1 style={{ fontWeight: "bold", color: "#B38B59" }} className="item-quantity">sold out</h1>
-                          
                         )
                       } else {
                         return (
@@ -301,20 +270,10 @@ export const Home = (props) => {
                     })()}
                   </div>
                 })}
-     
-
               </div>
-              
-
           }
-          
-          
     
         </div>
-  
-        
-
-        
       );
     
     }
