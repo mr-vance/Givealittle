1980dbfcc8521ce3a8bd397dc323a58af89cc0db
Thibello-Khaile <101095268+Thibello-Khaile@users.noreply.github.com>
2022-11-04 09:00:46 +0200

    Add files via upload

---
+++ Signup.js
@@ -0,0 +1,140 @@
+//getting all the relevant imports in place
+import React,{useState, useEffect, useRef} from 'react'
+//getting the relevant imports in place
+import {auth,fs} from '../Config/Config'
+import {Link} from 'react-router-dom'
+import {useHistory} from 'react-router-dom'
+import {shoppingCart} from 'react-icons-kit/feather/shoppingCart'
+import {Icon} from 'react-icons-kit'
+import logo from '../Images/logo.png'
+
+
+export const Signup = () => {
+
+    const history = useHistory();
+    //initializing the variables that will be used
+    const [fullName, setFullname]=useState('');
+    const [email, setEmail]=useState('');
+    const [password, setPassword]=useState('');
+
+    const [errorMsg, setErrorMsg]=useState('');
+    const [successMsg, setSuccessMsg]=useState('');
+
+    // defining current user function
+    function GetCurrentUser(){
+        const [user, setUser]=useState(null);
+        useEffect(()=>{
+            auth.onAuthStateChanged(user=>{
+                if(user){
+                    fs.collection('users').doc(user.uid).get().then(snapshot=>{
+                        setUser(snapshot.data().FullName);
+                    })
+                }
+                else{
+                    setUser(null);
+                }
+            })
+        },[])
+        return user;
+    }
+
+    const user = GetCurrentUser();
+
+    //defining the navigation bar function
+    function Navbar(){
+        const history = useHistory();
+
+        const handleLogout=()=>{
+            auth.signOut().then(()=>{
+                history.push('/login');
+            })
+        }
+    
+       
+    //Navigation bar
+        return (
+            <div className='navbar'>
+                <div className='leftside'>
+                    <div className='logo'>
+                        <Link to="/">
+                        <img src={logo} alt="logo"/>
+                        </Link>
+                    </div>
+                </div>
+                <div className='rightside'>
+                    {user&&<>
+                        
+                    </>}                     
+                                    
+                </div>
+            </div>
+    
+        )
+    }
+
+        //signing up authorization process. setting the success message when no error returned
+    const handleSignup=(e)=>{
+        e.preventDefault();
+        // console.log(fullName, email, password);
+        auth.createUserWithEmailAndPassword(email,password).then((credentials)=>{
+            console.log(credentials);
+            fs.collection('users').doc(credentials.user.uid).set({
+                FullName: fullName,
+                Email: email,
+                Password: password
+            }).then(()=>{
+                setSuccessMsg('Signup Successfull. You will now automatically get redirected to Login');
+                setFullname('');
+                setEmail('');
+                setPassword('');
+                setErrorMsg('');
+                setTimeout(()=>{
+                    setSuccessMsg('');
+                    history.push('/login');
+                },3000)
+            }).catch(error=>setErrorMsg(error.message));
+        }).catch((error)=>{
+            setErrorMsg(error.message)
+        })
+    }
+    //putting the navigation bar in place and designing the signup page and links to other pages.
+    return (
+        <>
+             <br></br>
+            <Navbar />
+        <div className='container'>
+            <br></br>
+            <br></br>
+            <h1>Sign Up</h1>
+            <hr></hr>
+            {successMsg&&<>
+                <div className='success-msg'>{successMsg}</div>
+                <br></br>
+            </>}
+            <form className='form-group' autoComplete="off" onSubmit={handleSignup}>
+                <label>Full Name</label>
+                <input type="text" className='form-control' required
+                onChange={(e)=>setFullname(e.target.value)} value={fullName}></input>
+                <br></br>
+                <label>Email</label>
+                <input type="email" className='form-control' required
+                 onChange={(e)=>setEmail(e.target.value)} value={email}></input>
+                <br></br>
+                <label>Password</label>
+                <input type="password" className='form-control' required
+                 onChange={(e)=>setPassword(e.target.value)} value={password}></input>
+                <br></br>
+                <div className='btn-box'>
+                    <span>Already have an account Login
+                    <Link to="login" className='link'> Here</Link></span>
+                    <button type="submit" className='btn btn-success btn-md'>SIGN UP</button>
+                </div>
+            </form>
+            {errorMsg&&<>
+                <br></br>
+                <div className='error-msg'>{errorMsg}</div>                
+            </>}
+        </div>
+        </>
+    )
+}
