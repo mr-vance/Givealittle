6bb009f9a8fa0aa32cc1464a901f0cdf46852eac
KgaugeloNalane <101181216+KgaugeloNalane@users.noreply.github.com>
2022-11-13 17:00:35 +0200

    Add files via upload

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,4 @@
 import React,{useState, useEffect, useRef} from 'react'
-//import { Navbar } from './Navbar'
 import { Products } from './Products'
 import {auth,fs} from '../Config/Config'
 import {Link} from 'react-router-dom'
@@ -7,18 +6,277 @@ import logo from '../Images/logo.png'
 import {Icon} from 'react-icons-kit'
 import {shoppingCart} from 'react-icons-kit/feather/shoppingCart'
 import {useHistory} from 'react-router-dom'
-import { AiOutlineHeart } from "react-icons/ai"
+import CategorySelector from './CategorySelector';
+import SearchSuggestion from './SearchSuggestion';
 
 
 export const Home = (props) => {
-
+//constant variables
     const [show, setShow] = useState(false);            //state for showing cart
     const [text, setText] = useState("");  
+
+     // state of products
+     const [products, setProducts]=useState([]);
 
    
     const searchRef = useRef();
     
     const [searchTerm, setSearchTerm] = useState("");
+
+// Category code-----------------------------------------------------------------------------------------------------------
+
+const [searchedProducts, setSearchedProducts] = useState([]);
+const [suggestionDropdown, setSuggestionDropdown] = useState(true);
+//state for collapsing the filter by category
+ const [isFilter, setIsFilter] = useState(false);
+ const handleFilter = () => {
+     if (isFilter){
+         setIsFilter(false);
+     }else{
+         setIsFilter(true)
+     }
+ }
+ 
+ const allCategories = ["All","Automotive", "Baby", "Beauty & Personal Care", "Books", "Cellphones & Wearables", "Computers & Electronics", "Gaming", "Fashion", "Health & Household", "Home & Appliances", "Liquor", "Office & Stationary", "Pets", "Sport & Training", "Toys", "TV Audio & Media"]
+
+ //State for the currently selected category
+ const [categoriesActivity, setAllCategoriesActivity] = useState([
+     {
+         categoryName: "All",
+         active: true,
+         products: []
+     },
+     {
+         categoryName: "Automotive",
+         active: false,
+         products: []
+     },
+     {
+         categoryName: "Baby",
+         active: false,
+         products: []
+     },
+     {
+         categoryName: "Beauty & Personal Care",
+         active: false,
+         products: []
+     },
+     {
+         categoryName: "Books",
+         active: false,
+         products: []
+     },
+     {
+         categoryName: "Cellphones & Wearables",
+         active: false,
+         products: []
+     },
+     {
+         categoryName: "Computers & Electronics",
+         active: false,
+         products: []
+     },
+     {
+         categoryName: "Gaming",
+         active: false,
+         products: []
+     },
+     {
+         categoryName: "Fashion",
+         active: false,
+         products: []
+     },
+     {
+         categoryName: "Health & Household",
+         active: false,
+         products: []
+     },
+     {
+         categoryName: "Home & Appliances",
+         active: false,
+         products: []
+     },
+     {
+         categoryName: "Liquor",
+         active: false,
+         products: []
+     },
+     {
+         categoryName: "Office & Stationary",
+         active: false,
+         products: []
+     },
+     {
+         categoryName: "Pets",
+         active: false,
+         products: []
+     },
+     {
+         categoryName: "Sport & Training",
+         active: false,
+         products: []
+     },
+     {
+         categoryName: "Toys",
+         active: false,
+         products: []
+     },
+     {
+         categoryName: "TV Audio & Media",
+         active: false,
+         products: []
+     }
+ ])
+
+
+ //currently active search category
+ const [searchActiveCategory, setSearchActiveCategory] = useState([{
+     categoryName: "All",
+     active: true,
+ },
+ {
+     categoryName: "Automotive",
+     active: false,
+ },
+ {
+     categoryName: "Baby",
+     active: false,
+ },
+ {
+     categoryName: "Beauty & Personal Care",
+     active: false,
+ },
+ {
+     categoryName: "Books",
+     active: false,
+ },
+ {
+     categoryName: "Cellphones & Wearables",
+     active: false,
+ },
+ {
+     categoryName: "Computers & Electronics",
+     active: false,
+ },
+ {
+     categoryName: "Gaming",
+     active: false,
+ },
+ {
+     categoryName: "Fashion",
+     active: false,
+ },
+ {
+     categoryName: "Health & Household",
+     active: false,
+ },
+ {
+     categoryName: "Home & Appliances",
+     active: false,
+ },
+ {
+     categoryName: "Liquor",
+     active: false,
+ },
+ {
+     categoryName: "Office & Stationary",
+     active: false,
+ },
+ {
+     categoryName: "Pets",
+     active: false,
+ },
+ {
+     categoryName: "Sport & Training",
+     active: false,
+ },
+ {
+     categoryName: "Toys",
+     active: false,
+ },
+ {
+     categoryName: "TV Audio & Media",
+     active: false,
+ }])
+
+ const [currentSearchCategory, setCurrentSearchCategory] = useState(searchActiveCategory.find(category => category.active).categoryName);
+ 
+ //handle user typing in search box
+ const handleSearchTermChange = (event) => {
+     setSearchTerm(event.target.value);
+   }
+
+ //function to search for an item in a given category
+ const searchCategory = (categoryName) => {
+     let category = categoriesActivity.find(category => category.categoryName == categoryName).products;
+     let tempArr = [];
+     for (let i in category){
+         let stringToSearch = category[i].Name.toLowerCase().replace(/\s/g, '')
+         
+         if (stringToSearch.includes(searchTerm.toLowerCase().replace(/\s/g, ''))){
+             tempArr = [...tempArr, category[i]]
+             setSearchedProducts([...tempArr]);
+         }
+        }
+      }
+        //useEffect for when the category selection changes
+    useEffect(() => {
+      if (searchTerm != ""){
+          searchCategory(currentSearchCategory)
+      }
+  }, [currentSearchCategory])
+
+  useEffect(() => {
+      searchCategory(currentSearchCategory)
+  },[searchTerm])
+
+  //used to show the category search dropdown menu
+  const [categoryDropdown, setCategoryDropdown] = useState(false);
+
+  const handleDropdown = () => {
+      if (categoryDropdown){
+          setCategoryDropdown(false);
+      }else {
+          setCategoryDropdown(true);
+      }
+
+  }
+
+  //function to split the string of categories by ,
+  const splitCategories = (categoriesString) => {
+      const categoriesArray = categoriesString.split(",");
+      return categoriesArray
+  }
+
+  const [currentActiveCategory, setCurrentActiveCategory] = useState("All")
+  //useEffect for the currenctly selected category
+  useEffect(() => {
+      setCurrentActiveCategory(categoriesActivity.find(category => category.active).categoryName)
+  },[categoriesActivity])
+
+  //useEffect for setting up the categories
+  useEffect(() => {
+      let tempArr = [...categoriesActivity]
+      for (let x in allCategories){
+          let currentCategory = allCategories[x]
+          for (let i in products){
+              let productCategories = splitCategories(products[i].Categories)
+              if(productCategories.includes(currentCategory)){
+                  let theCategory = tempArr.find(category => category.categoryName == currentCategory)
+                  theCategory.products.push(products[i])
+              }
+          }
+      }
+      setAllCategoriesActivity([...tempArr])        
+  },[products])
+
+
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+
+
+    
+
 
     function Navbar(){
         const history = useHistory();
@@ -28,7 +286,8 @@ export const Home = (props) => {
                 history.push('/login');
             })
         }
-    
+
+
        
     //Navigation bar
         return (
@@ -41,6 +300,7 @@ export const Home = (props) => {
                     </div>
                 </div>
                 <div className='rightside'>
+                    
     
                     {!user&&<>
                         <div><Link className='navlink' to="signup">SIGN UP</Link></div>
@@ -51,6 +311,8 @@ export const Home = (props) => {
                   placeholder="Search..."
                   ref={searchRef}
                 />
+
+                       
                 <button className="btnsearch" onClick={() => {
                   setSearchTerm(searchRef.current.value)
                 }}>
@@ -67,13 +329,7 @@ export const Home = (props) => {
                             <span className='cart-indicator'>{totalProducts}</span>
                         </div>
 
-                        <div className='cart-menu-btn'>
-                        <Link className='navlink' to="wishlist">
-                               
-                                        <AiOutlineHeart size={25}/>
-                        </Link>
-                        
-                    </div>
+                      
 
                         <div className='btn btn-danger btn-md'
                         onClick={handleLogout}>LOGOUT</div>
@@ -124,9 +380,7 @@ export const Home = (props) => {
     const user = GetCurrentUser();
     // console.log(user);
     
-    // state of products
-    const [products, setProducts]=useState([]);
-
+   
     // getting products function
     const getProducts = async ()=>{
         const products = await fs.collection('Products').get();
@@ -220,8 +474,8 @@ export const Home = (props) => {
               <h1 className="product-view-price">R{item.price}</h1>
               <div>
                 <input type="number" className="edtnum" placeholder="1" min='0' max={item.Quantity} />
-                <button className="cart-btn" onClick={() => addToCart(item)}>Add to cart</button>
-                <button className="cart-btn" onClick={() => addToWishlist(item)}>Add to wishlist</button>
+                <button className="btnsearch" onClick={() => addToCart(item)}>Add to cart</button>
+                <button className="btnsearch" onClick={() => addToWishlist(item)}>Add to wishlist</button>
               </div>
     
             </div>
@@ -230,50 +484,150 @@ export const Home = (props) => {
       }
 
 
-    
+  //----------------------------------------------------------------------------------------------------------------------------------  
 
 
 
       return (
         <div>
-          <Navbar />
-          {
-            show ? <div className="reviewdiv">
-              {text}
-            </div> :
-              <div className="bodydiv" >
-                {products.filter((item) => {
-                  if (searchTerm == "") {
-                    return item
-                  } else if (item.title.toLowerCase().includes(searchTerm.toLocaleLowerCase())) {
-                    return item
-                  }
-                }).map((item) => {
-                  return <div className="itemdiv" onClick={() => {
-                    ProductView(item)
-                  }}>
-                    <img src={item.url} alt="nope" />
-                    <div className="textdiv">
-                      <h1 className="itemname">{item.title}</h1>
+        <Navbar />
+        <SearchSuggestion 
+            searchedProducts={searchedProducts} 
+            searchTerm={searchTerm}
+            setSearchTerm={setSearchTerm}
+            suggestionDropdown={suggestionDropdown}
+            setSuggestionDropdown={setSuggestionDropdown}/>
+
+
+        {isFilter ? 
+        
+        <button className='filter' style={{backgroundColor: "#9ccc64"}} onClick={handleFilter}>Close Filter</button>
+        :
+        <button className='filter' onClick={handleFilter}>
+        Filter by Category
+        </button>
+    }
+
+        {isFilter ? 
+            <CategorySelector 
+            categoriesActivity={categoriesActivity} 
+            setAllCategoriesActivity={setAllCategoriesActivity}
+            setCategoryDropdown={setCategoryDropdown}/>
+            :null
+        }
+        
+
+
+        {searchedProducts.length != 0 && searchTerm != "" ?
+            <div>
+                {searchTerm != "" ?
+                    <h6 className='category-search-heading'>({searchedProducts.length}) Results for "{searchTerm}" in <span className='search-result-category'>{currentSearchCategory}</span></h6>
+                    : null
+                }
+                <div className='bodydiv'>
+            {searchedProducts.map((item, itemIndex) => {
+                return (
+                    <div key={itemIndex} className="itemdiv" onClick={() => {
+                        ProductView(item)
+                    }}>
+                        <img src={item.url} alt="nope" />
+                        <div className="textdiv">
+                            <h1 className="itemname">{item.title}</h1>
+                        </div>
+                        <h1 className="itemprice">R{item.Price}</h1>
+                        {(() => {
+                            if (item.Quantity == 0) {
+                                return (
+                                    <h1 style={{ fontWeight: "bold", color: "#B38B59" }} className="item-quantity">sold out</h1>
+                                )
+                            } else {
+                                return (
+                                    <h1 className="item-quantity">in stock</h1>
+                                )
+                            }
+                        })()}
                     </div>
-                    <h1 className="itemprice">R{item.price}</h1>
-                    {(() => {
-                      if (item.Quantity == 0) {
+                )
+            })}
+            </div>
+            </div>
+            : null
+        }
+        
+            
+
+        {   currentActiveCategory != "All" ? 
+            <div>
+                <h6 className='category-heading'>{currentActiveCategory + " (" + categoriesActivity.find(category => category.categoryName == currentActiveCategory).products.length + ")"}</h6>
+
+            <div className='bodydiv'>
+            {categoriesActivity.find(category => category.categoryName == currentActiveCategory).products.map((item, itemIndex) => {
+                return (
+                    <div key={itemIndex} className="itemdiv" onClick={() => {
+                        ProductView(item)
+                    }}>
+                        <img src={item.url} alt="nope" />
+                        <div className="textdiv">
+                            <h1 className="itemname">{item.title}</h1>
+                        </div>
+                        <h1 className="itemprice">R{item.price}</h1>
+                
+                        {(() => {
+                            if (item.Quantity == 0) {
+                                return (
+                                    <h1 style={{ fontWeight: "bold", color: "#B38B59" }} className="item-quantity">sold out</h1>
+                                )
+                            } else {
+                                return (
+                                    <h1 className="item-quantity">in stock</h1>
+                                )
+                            }
+                        })()}
+                    </div>
+                )
+            })}
+            </div>
+            </div>
+            :null
+        }
+        
+
+
+        <h6 className='category-heading'>All products{" (" + categoriesActivity.find(category => category.categoryName == "All").products.length + ")"}</h6>
+        {
+            show ? <div className="reviewdiv">
+                {text}
+            </div> :
+                <div className="bodydiv" >
+                    {products.map((item, indx) => {
                         return (
-                          <h1 style={{ fontWeight: "bold", color: "#B38B59" }} className="item-quantity">sold out</h1>
+                        <div key={indx} className="itemdiv" onClick={() => {
+                            ProductView(item)
+                        }}>
+                            <img src={item.url} alt="nope" />
+                            <div className="textdiv">
+                                <h1 className="itemname">{item.title}</h1>
+                            </div>
+                            <h1 className="itemprice">R{item.price}</h1>
+            
+                            {(() => {
+                                if (item.Quantity == 0) {
+                                    return (
+                                        <h1 style={{ fontWeight: "bold", color: "#B38B59" }} className="item-quantity">sold out</h1>
+                                    )
+                                } else {
+                                    return (
+                                        <h1 className="item-quantity">in stock</h1>
+                                    )
+                                }
+                            })()}
+                        </div>
                         )
-                      } else {
-                        return (
-                          <h1 className="item-quantity">in stock</h1>
-                        )
-                      }
-                    })()}
-                  </div>
-                })}
-              </div>
-          }
-    
-        </div>
-      );
+                    })}
+                </div>
+        }
+
+    </div>
+);
     
     }
